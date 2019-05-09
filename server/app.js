24989d67378bb003241a061b80f3cb71adb362f4
DionGrimm <31693758+DionGrimm@users.noreply.github.com>
2019-05-09 11:18:13 +0200

    Updated the generater and the ai

--- server/app.js
+++ server/app.js
@@ -3,18 +3,27 @@ const app = express()
 const port = 3000
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-var ioreq = require("socket.io-request");
+const ioreq = require("socket.io-request");
 const fs = require('fs');
 
 // brain.js
 let testRoutes = [
+  {weather:0,temp:23,foot:12,car:0,step:0,bike:0,scooter:8},
+  {weather:0,temp:23,foot:5,car:22,step:0,bike:0,scooter:3},
+  {weather:0,temp:23,foot:6,car:0,step:0,bike:22,scooter:5}
+  /* old  test routes
   {temp: 14, wind: 5, rain: 60, departure: 600, congestion: 3, transfers: 7, car: 800, bicycle: 15, foot: 15},
   {temp: 4, wind: 7, rain: 60, departure: 700, congestion: 800, transfers: 1, car: 20, bicycle: 5, foot: 20},
   {temp: 13, wind: 5, rain: 60, departure: 600, congestion: 3, transfers: 0, car: 35, bicycle: 0, foot: 3}
+  */
 ]
 
 // Learning data
-let data = [
+let data = JSON.parse(fs.readFileSync('data/generated/aiData1557354306.json', 'utf8'));
+console.log(data)
+
+/* Old test data
+[
     {input: {temp: 20, wind: 5, rain: 60, departure: 600, congestion: 7, transfers: 1, car: 20, bicycle: 5, foot: 3}, output: [0]},
     {input: {temp: 13, wind: 6, rain: 70, departure: 600, congestion: 4, transfers: 0, car: 38, bicycle: 0, foot: 3}, output: [1]},
     {input: {temp: 18, wind: 3, rain: 50, departure: 630, congestion: 5, transfers: 0, car: 30, bicycle: 0, foot: 3}, output: [1]},
@@ -22,11 +31,13 @@ let data = [
     {input: {temp: 14, wind: 5, rain: 60, departure: 600, congestion: 3, transfers: 3, car: 50, bicycle: 15, foot: 15}, output: [0]},
     {input: {temp: 4, wind: 7, rain: 60, departure: 700, congestion: 8000, transfers: 1, car: 20, bicycle: 5, foot: 20}, output: [0]}
 ]
+*/
+//Route 1: Zonnig, 18 graden, maandag, druk verkeer
 
 const brain = require('brain.js')
 const network = new brain.NeuralNetwork()
 console.log("created a neural net")
-let stats = network.train(data)
+let stats = network.train(data.data)
 console.log("finished training...")
 console.log(stats)
 
@@ -34,6 +45,8 @@ console.log(stats)
 // console.log(network.run(testRoutes[0]))
 // console.log(network.run(testRoutes[1]))
 // console.log(network.run(testRoutes[2]))
+
+// Find the best result
 let results = []
 
 for (let index = 0; index < testRoutes.length; index++) {
@@ -61,8 +74,11 @@ function indexOfMax(arr) {
 
 console.log(results)
 let suggestedRoute = indexOfMax(results)
+//--
 
-app.use(express.static('public'))
+app.get('/', function(req, res){
+  res.send('Backend ai server is running');
+});
 
 io.on('connection', function(socket) {
   console.log('an user connected')
--- server/generate.js
+++ server/generate.js
@@ -1,14 +1,95 @@
-console.log("-------AI Dummy data generator-------");
+const fs = require('fs');
+
 let timestamp = Math.round(+new Date()/1000);
 let filename = "data/generated/aiData"+timestamp+".json";
 
-content = //Generate data with for loop based on persona's
+
+// Datasets van persona
+let persona = 
+{
+  username:"klaasdeboer",
+  password:"1234",
+  name:"Klaas de Boer",
+  employer:"Nationale Nederlanden",
+  birth:"04-03-1992",
+  age:27,
+  license:false,
+  firstChoice:"scooter",
+  niceWeather:"bike",
+  workDays:[1,2,3,4], //1 = monday etc.
+  routes:[{title:"Huis - Werk",from:"Parklaan 11",to:"Vrijkade 23"},{title:"Vrijdag route",from:"Eurolaan 4",to:"Vrijkade 23"}]
+}
+//Script die op basis van voorbeeld context zegt wat voor route de persoon neemt
+let loopTimes = 20
+let weather = "sunny"
+let temp = 22
+let maxFoot = 20, maxCar = 15, maxStep = 0, maxBike = 30, maxScooter = 30
+let firstChoice = 4
+
+let dummyData = [
+  {input:{weather:0,temp:22,foot:15,car:0,step:0,bike:0,scooter:10},output:[1]},
+  {input:{weather:0,temp:22,foot:3,car:25,step:0,bike:0,scooter:0},output:[0]},
+  {input:{weather:0,temp:22,foot:3,car:0,step:0,bike:25,scooter:8},output:[0.5]},
+
+  {input:{weather:1,temp:12,foot:2,car:20,step:0,bike:0,scooter:0},output:[0.5]},
+  {input:{weather:1,temp:12,foot:3,car:10,step:0,bike:15,scooter:0},output:[0]},
+  {input:{weather:1,temp:12,foot:3,car:0,step:0,bike:5,scooter:20},output:[1]},
+
+  {input:{weather:2,temp:8,foot:1,car:30,step:0,bike:0,scooter:0},output:[1]},
+  {input:{weather:2,temp:8,foot:3,car:20,step:0,bike:0,scooter:7},output:[0.5]},
+  {input:{weather:2,temp:8,foot:7,car:8,step:0,bike:15,scooter:0},output:[0]},
+]
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let data = []
+
+for (let index = 0; index < dummyData.length; index++) {
+  const element = dummyData[index]
+
+  for (let index = 0; index < 10; index++) {
+    let variation = {input:{},output:[]}
+
+    variation.input.weather = element.input.weather
+    variation.input.temp = getRandomInt(element.input.temp-2,element.input.temp+2)
+    variation.input.foot = getRandomInt(element.input.foot-2,element.input.foot+2)
+    variation.input.car = getRandomInt(element.input.car-2,element.input.car+2)
+    variation.input.step = getRandomInt(element.input.step-2,element.input.step+2)
+    variation.input.bike = getRandomInt(element.input.bike-2,element.input.bike+2)
+    variation.input.scooter = getRandomInt(element.input.scooter-2,element.input.scooter+2)
+    variation.output = element.output
+
+    data.push(variation)
+  }
+  data.push(element)
+}
+
+persona.data = data
+
+let content = persona;//Generate data with for loop based on persona's
 
 content = JSON.stringify(content);  
-var fs = require('fs');
+
+console.log("-------AI Dummy data generator-------");
 
 fs.appendFile(filename, content, function (err) {
   if (err) throw err;
 
   console.log("Data generated, file saved to: "+ filename);
 });
+
+/*
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`What's your name?`, (name) => {
+  console.log(`Hi ${name}!`)
+  readline.close()
+})
+*/
