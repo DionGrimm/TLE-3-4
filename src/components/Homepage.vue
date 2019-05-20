faf942f222ed18b9d106e65da05738a46ad81460
Justin de Ruiter <djjustin13@gmail.com>
2019-05-20 20:46:32 +0200

    Moved menu over te seperate component

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -5,21 +5,8 @@
       <div class="main-background"></div>
     </div>
     <div class="content-wrapper">
-      <!-- menu -->
-      <Slide :crossIcon="false">
-        <div>
-          <h1>{{profile.name}}</h1>
-          <span>{{profile.employer}}</span>
-        </div>
-        <router-link to='/homepage'><span>Reizen</span></router-link>
-        <router-link to='/routes'><span>Routes</span></router-link>
-        <router-link to='/maps'><span>Kaart</span></router-link>
-        <router-link to='/homepage'><span>Probleem melden</span></router-link>
-        <router-link to='/homepage'><span>Help</span></router-link>
-      </Slide>
-
+      <Menu v-bind:profile="profile"/>
       <div class="transparentcontainer">
-
         <img src="@/assets/logo_leaseplan.png" class="logo_leaseplan">
 
         <div class="input-item">
@@ -38,8 +25,13 @@
 </template>
 
 <script>
+import Menu from './Menu'
+
 export default {
   name: 'Homepage',
+  components: {
+    Menu
+  },
   data() {
       return {
         socket : io('localhost:3000'),
---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <Slide :crossIcon="false">
+        <div>
+          <h1>{{profile.name}}</h1>
+          <span>{{profile.employer}}</span>
+        </div>
+        <router-link to='/homepage'><span>Reizen</span></router-link>
+        <router-link to='/routes'><span>Routes</span></router-link>
+        <router-link to='/maps'><span>Kaart</span></router-link>
+        <router-link to='/homepage'><span>Probleem melden</span></router-link>
+        <router-link to='/homepage'><span>Help</span></router-link>
+      </Slide>
+</template>
+
+<script>
+
+export default {
+  name: 'Menu',
+  props:{
+    profile : Object
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.main{}
+
+</style>
