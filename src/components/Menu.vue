1006f2c39c94876c64fa95f5b2169c8142543b26
RickSchijvenaars <36455350+RickSchijvenaars@users.noreply.github.com>
2019-05-21 17:13:55 +0200

    Added icons in menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,11 +4,26 @@
           <h1>{{profile.name}}</h1>
           <span>{{profile.employer}}</span>
         </div>
-        <router-link to='/homepage'><span>Reizen</span></router-link>
-        <router-link to='/routes'><span>Routes</span></router-link>
-        <router-link to='/maps'><span>Kaart</span></router-link>
-        <router-link to='/homepage'><span>Probleem melden</span></router-link>
-        <router-link to='/homepage'><span>Help</span></router-link>
+        <router-link to='/homepage'>
+          <i class="fas fa-map-marked-alt"></i>
+          <span>Reizen</span>
+        </router-link>
+        <router-link to='/routes'>
+          <i class="fas fa-route"></i>
+          <span>Routes</span>
+        </router-link>
+        <router-link to='/maps'>
+          <i class="fas fa-map"></i>
+          <span>Kaart</span>
+        </router-link>
+        <router-link to='/homepage'>
+          <i class="fas fa-exclamation-circle"></i>
+          <span>Probleem melden</span>
+        </router-link>
+        <router-link to='/homepage'>
+          <i class="fas fa-question-circle"></i>
+          <span>Help</span>
+        </router-link>
       </Slide>
 </template>
 
